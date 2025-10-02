1449bff660173b70c173acfabe0237da27bd27a6
tatojastine-n <jajanicoletato@gmail.com>
2025-10-02 21:48:22 +0800

    TIME CONVERSION.cs

--- TIME CONVERSION.cs
+++ TIME CONVERSION.cs
@@ -1,30 +1,61 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
+class Result
+{
 
-/*
- * Complete the 'timeConversion' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING s as parameter.
- */
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
 
-string timeConversion(string s) {
+    public static string timeConversion(string s)
+    {
+        string hoursStr = s.Substring(0, 2);
+        string minsStr = s.Substring(3, 2);
+        string secsStr = s.Substring(6, 2);
+        string period = s.Substring(8, 2);
+        int hours = int.Parse(hoursStr);
+        if (period == "PM" && hours != 12)
+        {
+            hours += 12;
+        }
+        else if (period == "AM" && hours == 12)
+        {
+            hours = 0;
+        }
+        string result = hours.ToString("D2") + ":" + minsStr + ":" + secsStr;
+        return result;
+    }
 
 }
 
-int main()
+class Solution
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-    string s;
-    getline(cin, s);
+        string s = Console.ReadLine();
 
-    string result = timeConversion(s);
+        string result = Result.timeConversion(s);
 
-    fout << result << "\n";
+        textWriter.WriteLine(result);
 
-    fout.close();
-
-    return 0;
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
